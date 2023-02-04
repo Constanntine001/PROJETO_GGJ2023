49a26dc8ba5ae0c58534477b83d81cebc64fdfc4
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2023-02-04 20:10:05 -0300

    NewFuncsDragDetector

--- Assets/Scripts/DragAndDropScripts/DragDetector.cs
+++ Assets/Scripts/DragAndDropScripts/DragDetector.cs
@@ -14,17 +14,51 @@ public class DragDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     [SerializeField] ItemSO infoItem;
 
     GameObject dragCanvas;
-    GameObject DragAndDropGhost;
+    GameObject CanvasDropGhost;
+    GameObject WorldDropGhost;
+    GameObject Bonsai_Bowl;
 
     public void Start()
     {
         // Acha o canvas que será utilizado para arrastar o objeto sobre a UI
         dragCanvas = GameObject.Find("CANVAS_DRAG_DROP");
+        Bonsai_Bowl = GameObject.Find("BONSAI_BOWL");
     }
 
     public void DragAndDropInformation(ItemSO input_infoItem)
     {
         infoItem = input_infoItem;
+    }
+
+    void CriarCanvasDropGhost()
+    {
+        CanvasDropGhost = new GameObject("ItemDragDrop", typeof(RectTransform), typeof(Image));
+        CanvasDropGhost.transform.SetParent(dragCanvas.transform);
+        var imageComponent = CanvasDropGhost.GetComponent<Image>();
+        imageComponent.sprite = infoItem.Sprite;
+        CanvasDropGhost.transform.localScale = CanvasDropGhost.transform.localScale * infoItem.Scale * infoItem.UIScale;
+        imageComponent.color = new Color(1f, 0.4f, 0.4f, 0.7f);
+    }
+
+    void CriarWorldDropGhost()
+    {
+        WorldDropGhost = new GameObject("ItemDragDrop", typeof(SpriteRenderer));
+        WorldDropGhost.transform.localScale = new Vector3(1, 1, 1);
+        var imgItemBonsai = WorldDropGhost.GetComponent<SpriteRenderer>();
+        imgItemBonsai.sprite = infoItem.Sprite;
+        imgItemBonsai.transform.localScale = imgItemBonsai.transform.localScale * infoItem.Scale;
+        WorldDropGhost.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void CriarItemBonsai()
+    {
+        var NovoItemBonsai = new GameObject("ItemDragDrop", typeof(SpriteRenderer));
+        NovoItemBonsai.transform.localScale = new Vector3(1, 1, 1);
+        var imgItemBonsai = NovoItemBonsai.GetComponent<SpriteRenderer>();
+        imgItemBonsai.sprite = infoItem.Sprite;
+        imgItemBonsai.transform.localScale = imgItemBonsai.transform.localScale * infoItem.Scale;
+        NovoItemBonsai.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        NovoItemBonsai.transform.SetParent(Bonsai_Bowl.transform);
     }
 
     // Update is called once per frame
@@ -37,11 +71,8 @@ public class DragDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
             {
                 // Flag arrastando ativa
                 arrastando = true;
-                DragAndDropGhost = new GameObject("ItemDragDrop", typeof(RectTransform), typeof(Image));
-                DragAndDropGhost.transform.SetParent(dragCanvas.transform);
-                var imageComponent = DragAndDropGhost.GetComponent<Image>();
-                imageComponent.sprite = infoItem.Sprite;
-                imageComponent.color = new Color(1f, 1f, 1f, 0.4f);
+                CriarCanvasDropGhost();
+                CriarWorldDropGhost();
             }
 
         }
@@ -49,14 +80,17 @@ public class DragDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         // Se estiver arrastando e o mouse estiver ainda clicado
         if(arrastando && Input.GetMouseButton(0))
         {
-            DragAndDropGhost.transform.position = Input.mousePosition;
+            CanvasDropGhost.transform.position = Input.mousePosition;
+
         }
         // SE ESTIVER ARRASTANDO MAS BTN NAO CLICADO
         else if(arrastando)
         {
             //ARRASTE ACABOU
             arrastando = false;
-            Destroy(DragAndDropGhost);
+            Destroy(CanvasDropGhost);
+
+            CriarItemBonsai();
 
             //... codigo de colliders talvez?
         }
