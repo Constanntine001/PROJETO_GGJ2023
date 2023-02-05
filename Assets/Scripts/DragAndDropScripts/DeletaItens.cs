c39c2328a3cf2da6ad3d02cbbdb66b0e4952abbb
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2023-02-05 05:58:04 -0300

    MainScene

--- Assets/Scripts/DragAndDropScripts/DeletaItens.cs
+++ Assets/Scripts/DragAndDropScripts/DeletaItens.cs
@@ -6,6 +6,8 @@ public class DeletaItens : MonoBehaviour
 {
     GameManager gm;
 
+    [SerializeField] AudioSource deletaItens;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +18,33 @@ public class DeletaItens : MonoBehaviour
     public void Deleta_Itens()
     {
         var childCount = gm.ACTIVE_BONSAI.transform.childCount;
+
+        if(childCount > 0)
+        {
+            deletaItens.Play();
+        }
+
         for(int i = 0; i < childCount; i++)
         {
             Destroy(gm.ACTIVE_BONSAI.transform.GetChild(i).gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null) 
+            {
+                if(hit.collider.gameObject.name == "DELETA_ITEMS(PA)")
+                {
+                    Deleta_Itens();
+                }
+            }
+        }
     }
 }
